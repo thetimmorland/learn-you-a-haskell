e2eb43829af4624b32323965ccbb1cb8c037fdf4
Timothy John Morland <me@timothy-morland.com>
2020-07-04 10:59:20 -0400

    finish recursion chapter

---
+++ recursion.hs
@@ -0,0 +1,31 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+replicate' :: (Num n, Ord n) => n -> x -> [x]
+replicate' 0 _ = []
+replicate' n x = x : replicate' (n-1) x
+
+take' :: (Num n, Ord n) => n -> [x] -> [x]
+take' n _ | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x:xs)
+    | x == a = True
+    | otherwise = elem' a xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = 
+    let smallerSorted = quicksort [a | a <- xs, a <= x]
+        biggerSorted = quicksort [a | a <- xs, a > x]
+    in smallerSorted ++ [x] ++ biggerSorted
