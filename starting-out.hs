b8bf5835f49d40e7b434d19f1873ba24636a8423
Timothy John Morland <me@timothy-morland.com>
2020-07-04 08:39:23 -0400

    Finish Chapter 1

---
+++ starting-out.hs
@@ -0,0 +1,24 @@
+doubleMe x = x + x
+doubleUs x y = doubleMe 7 + doubleMe y
+
+doubleSmallNumber x = if x > 100
+    then x
+    else x*2
+
+list = 1 : 2: 3 : []
+
+extractIdx l i = l !! i
+
+range = [1..100]
+odd_ = [1,3..]
+
+congruent3mod7 = [ x | x <- [1..50], x `mod` 7 == 3]
+
+-- fizzBuzz = [ fb x | x <- [1..100] ] where
+--     fb 3 = "fizz"
+--     fb 5 = "buzz"
+--     fb 15 = "fizbuzz"
+--     fb x = x
+
+xxs = [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]]  
+evenXss = [ [ x | x <- xs, even x ] |  xs <- xxs ]
